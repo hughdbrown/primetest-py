ae5247a2af8b66fe76a0f7ba0af998405985f722
Hugh Brown <hughdbrown@yahoo.com>
2023-09-08 08:22:32 -0600

    Fix off-by-1 error

--- primetest.py
+++ primetest.py
@@ -11,18 +11,21 @@ def sieve_of_eratosthenes(count):
         upper = int(count ** 0.5) + 1 
         for i in range(2, upper):
             if values[i]:
-                s = slice(i*i, count, i)
-                x = (count - i*i + i - 1) // i
+                s = slice(i*i, count + 1, i)
+                x = (count - i*i + i) // i
                 values[s] = [False] * x
+                #for j in range(i * i, count + 1, i):
+                #    values[j] = False
         return values
 
 def prime_count(count):
-    prime_mask = sieve_of_eratosthenes(count)
-    return sum(prime_mask)
+    primes = [i for (i, v) in enumerate(sieve_of_eratosthenes(count)) if v]
+    # print(primes)
+    return len(primes)
 
 if __name__ == '__main__':
     for i in (10, 100, 1000, 10000, 100000, 1000*1000, 10*1000*1000, 100*1000*1000):
         start = datetime.now()
-        prime_count(i)
+        x = prime_count(i)
         end = datetime.now()
-        print(f"{i}: {end - start}")
+        print(f"{i}/{x}: {end - start}")
